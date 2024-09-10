a83dd0ddbdc51dd4460f3bdd475fd07cf2753d36
William <wangwilliam2010@gmail.com>
2024-09-10 17:32:38 +1000

    de2

--- Building_Survey/Room_Booking/Controllers/HomeController.cs
+++ Building_Survey/Room_Booking/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+// what is going on? 
 namespace Room_Booking.Controllers
 {
     public class HomeController : Controller
